2b1881769c8a79ac9a20ca4af177ed81ec23dede
Almudena Espinosa <almu.e.c@gmail.com>
2023-06-05 19:33:02 +0200

    aspectos legales

---
+++ server/src/politicaPrivacidad.php
@@ -0,0 +1,173 @@
+<?
+    //Propago sesión
+    include "sesion.php";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?
+    //Head
+    include "../public/head.html";
+    ?>
+</head>
+
+<body>
+    <?
+    // Cabecera
+    include "../public/cabecera.html";
+    ?>
+    <div class="container">
+        <div class="mb-4">
+            <h1 class="text-center mb-5"><strong>-- Política de Privacidad --</strong></h1>
+            <p>Según lo dispuesto en el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo, de 27 de abril de
+                2016, relativo a la protección de las personas físicas en lo que respecta al tratamiento de datos
+                personales, por el que se deroga la directiva 95/46/CE (en adelante, RGPD), de la Ley 34/2002, de 11 de
+                julio, de servicios de la sociedad de la información y de comercio electrónico (en adelante, LSSI-CE) y
+                de la Ley Orgánica 3/2018, de Protección de Datos Personales y garantía de los derechos digitales
+                ALMUDENA ESPINOSA CUEVAS garantiza la protección y confidencialidad de los datos personales, de
+                cualquier tipo que nos proporcionen nuestros clientes, de acuerdo con lo dispuesto en el Reglamento
+                General de Protección de Datos de Carácter Personal.</p>
+
+            <p>La Política de Protección de Datos de ALMUDENA ESPINOSA CUEVAS descansa en el principio de
+                responsabilidad proactiva, según el cual el Responsable del tratamiento es responsable del cumplimiento
+                del marco normativo y jurisprudencial, siendo capaz de demostrarlo ante las autoridades de control
+                correspondientes.</p>
+
+            <p>>Los datos facilitados serán tratados en los términos establecidos en el RGPD, en este sentido ALMUDENA
+                ESPINOSA CUEVAS ha adoptado los niveles de protección que legalmente se exigen, y ha instalado todas las
+                medidas técnicas a su alcance para evitar la pérdida, mal uso, alteración, acceso no autorizado por
+                terceros, expuestos a continuación. No obstante, el usuario debe ser consciente de que las medidas de
+                seguridad en Internet no son inexpugnables.</p>
+
+            <h2>Responsable del tratamiento: ¿Quiénes somos?</h2>
+
+            <p>Denominación: ALMUDENA ESPINOSA CUEVAS</p>
+
+            <p>CIF: 47129103W</p>
+
+            <p>Dirección: Plaza Juan Jiménez Aguilar, 16004 Cuenca, Cuenca</p>
+
+            <p>Teléfono: 612340263</p>
+
+            <p>Correo electrónico: contacto@megalopolis.es</p>
+
+            <h2>Finalidad del tratamiento: ¿Para qué usaremos sus datos?</h2>
+
+            <p>Todos los datos facilitados por nuestros clientes y/o visitantes en la web de ALMUDENA ESPINOSA CUEVAS o
+                a su personal, serán incluidos en registro de actividades de tratamiento de datos de carácter personal,
+                creado y mantenido bajo la responsabilidad de ALMUDENA ESPINOSA CUEVAS, imprescindibles para prestar los
+                servicios solicitados por los usuarios, o para resolver las dudas o cuestiones planteadas por nuestros
+                visitantes. Nuestra política es no elaborar perfiles sobre los usuarios de nuestros servicios.</p>
+
+            <h2>Legitimidad del tratamiento: ¿Por qué necesitamos sus datos?</h2>
+            <ol type="a)">
+                <li><strong>Relación contractual:</strong> Es la que aplica cuando compra uno de nuestros productos o contrata alguno de
+                    nuestros servicios.</li>
+                <li><strong>Interés legítimo:</strong> Para atender a las consultas y reclamaciones que nos plantee y para gestionar el
+                    cobro de las cantidades adeudadas.</li>
+                <li><strong>Su consentimiento:</strong> Si es usuario de nuestra web, mediante la marcación de la casilla que figura en
+                    el formulario de contacto, nos autoriza a que le remitamos las comunicaciones necesarias para dar
+                    respuesta a la consulta o solicitud de información planteada.</li>
+            </ol>
+            <h2>Destinatarios: ¿Con quién compartimos sus datos?</h2>
+
+            <p>No cedemos sus datos personales a nadie, a excepción de aquellas entidades públicas o privadas a las
+                cuales estemos obligados a facilitar sus datos personales con motivo del cumplimiento de alguna ley. Por
+                poner un ejemplo, la Ley Tributaria obliga a facilitar a la Agencia Tributaria determinada información
+                sobre operaciones económicas que superen una determinada cantidad.</p>
+
+            <p>En el caso de que, al margen de los supuestos comentados, necesitemos dar a conocer su información
+                personal a otras entidades, le solicitaremos previamente su permiso a través de opciones claras que le
+                permitirán decidir a este respecto.</p>
+
+            <h2>Comunicación: ¿Dónde podríamos enviar tus datos?</h2>
+
+            <p>No realizaremos transferencias internacionales de sus datos personales para ninguna de las finalidades
+                indicadas.</p>
+
+            <h2>Conservación: ¿Cuánto tiempo mantendremos tus datos?</h2>
+
+            <p>Solo conservaremos sus datos personales durante el tiempo que resulte necesario para lograr los fines
+                para los que fueron recabados. A la hora de determinar el oportuno periodo de conservación, examinamos
+                los riesgos que conlleva el tratamiento, así como nuestras obligaciones contractuales, legales y
+                normativas, las políticas internas de conservación de datos y nuestros intereses de negocio legítimos
+                descritos en el presente Aviso de Privacidad y Política de Cookies.</p>
+
+            <p>En este sentido, ALMUDENA ESPINOSA CUEVAS conservará los datos personales una vez terminada su relación
+                con Ud., debidamente bloqueados, durante el plazo de prescripción de las acciones que pudieran derivarse
+                de la relación mantenida con el interesado.</p>
+
+            <p>Una vez bloqueados, sus datos resultarán inaccesibles para ALMUDENA ESPINOSA CUEVAS, y no serán tratados
+                excepto para su puesta a disposición a las Administraciones públicas, Jueces y Tribunales, para la
+                atención de las posibles responsabilidades nacidas de los tratamientos, así como para el ejercicio y
+                defensa de reclamaciones ante la Agencia Española de Protección de Datos.</p>
+
+            <h2>Seguridad: ¿Cómo vamos a proteger sus datos?</h2>
+
+            <p>Empleamos todos los esfuerzos razonables para mantener la confidencialidad de la información personal que
+                se trate en nuestros sistemas. Mantenemos estrictos niveles de seguridad para proteger los datos de
+                carácter personal que procesamos frente a pérdidas fortuitas y a accesos, tratamientos o revelaciones no
+                autorizados, habida cuenta del estado de la tecnología, la naturaleza y los riesgos a que están
+                expuestos los datos. No obstante, no podemos responsabilizarnos del uso que Ud. haga de los datos
+                (incluido usuario y contraseña) que utilice en nuestra web. Nuestro personal sigue estrictas normas de
+                privacidad y en el caso de que contratemos a terceros para prestar servicios de soporte, les exigimos
+                que acaten las mismas normas y nos permitan auditarles para verificar su cumplimiento.</p>
+
+            <h2>Sus derechos: ¿Qué derechos puede ejercer como interesado?</h2>
+
+            <p>Le informamos que podrá ejercer los siguientes derechos:</p>
+            <ol>
+                <li> Derecho de acceso a sus datos personales, para saber cuáles están siendo objeto de tratamiento y
+                    las
+                    operaciones de tratamiento llevadas a cabo con ellos;</li>
+                <li>Derecho de rectificación de cualquier dato personal inexacto;</li>
+                <li>Derecho de supresión de sus datos personales, cuando esto sea posible (por ejemplo, por imperativo
+                    legal);</li>
+                <li>Derecho de limitación del tratamiento de sus datos personales cuando la exactitud, la legalidad o la
+                    necesidad del tratamiento de los datos resulte dudosa, en cuyo caso, podremos conservarlos para el
+                    ejercicio o la defensa de reclamaciones.</li>
+                <li>Derecho de oposición al tratamiento de sus datos personales, cuando la base legal que nos habilite
+                    para
+                    su tratamiento de las indicadas sea nuestro interés legítimo. ALMUDENA ESPINOSA CUEVAS dejará de
+                    tratar
+                    tus datos salvo que tenga un interés legítimo o sea necesario para la defensa de reclamaciones.</li>
+                <li>Derecho a la portabilidad de sus datos, cuando la base legal que nos habilite para su tratamiento
+                    sea la
+                    existencia de una relación contractual o su consentimiento.</li>
+                <li>Derecho a revocar el consentimiento otorgado a ALMUDENA ESPINOSA CUEVAS</li>
+            </ol>
+            <p>Para ejercitar sus derechos, puede hacerlo de manera gratuita y en cualquier momento, contactando con
+                nosotros en la dirección C/ Miguel Hernández 39 1D, 16004, Cuenca, Cuenca, o escribiendo un correo a
+                contacto@cidramarketing.es, adjuntando siempre copia del DNI.</li>
+
+            <h2>Tutela de derechos: ¿Dónde puede formular una reclamación?</h2>
+
+            <p>En caso de que entienda que sus derechos han sido desatendidos por nuestra entidad, puede formular una
+                reclamación en la Agencia Española de Protección de Datos, a través de alguno de los medios siguientes:
+
+            <ul>
+                <li>Sede electrónica: https://www.aepd.es</li>
+                <li>Correo postal: Agencia Española de Protección de Datos, C/ Jorge Juan, 6, 28001, Madrid</li>
+                <li>Teléfono: 901.100.099 y 912.663.517</li>
+            </ul>
+
+            <p>Formular una reclamación en la Agencia Española de Protección de Datos no conlleva ningún coste y no es
+                necesaria la asistencia de abogado ni procurador.</p>
+
+            <h2>Actualizaciones: ¿Qué cambios puede haber en esta Política de Privacidad?</h2>
+
+            <p>ALMUDENA ESPINOSA CUEVAS se reserva el derecho a modificar la presente política para adaptarla a
+                novedades legislativas o jurisprudenciales que puedan afectar el cumplimiento de la misma.</p>
+        </div>
+
+    </div>
+
+    <?
+    // Pie de página
+    include "../public/footer.html";
+    ?>
+</body>
+
+</html>
