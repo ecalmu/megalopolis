2c060809db09499e2e1a489373f1fe64f41ac8b6
Almudena Espinosa <almu.e.c@gmail.com>
2023-06-11 06:10:52 +0200

    recordar credenciales login

--- server/src/login.php
+++ server/src/login.php
@@ -5,6 +5,7 @@
     //Head
     include "../public/head.html";
     ?>
+	<script type="text/javascript" src="js/login.js"></script>
 </head>
 
 <body class="login">
@@ -16,22 +17,22 @@
 				<h3>Iniciar sesión</h3>
 			</div>
 			<div class="card-body">
-				<form action="/inicioSesion" method="post">
+				<form action="/inicioSesion" method="post" id="formulario">
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
-						<input type="text" class="form-control" placeholder="Usuario" name="nombre" required>
+						<input type="text" class="form-control" placeholder="Usuario" name="nombre" id="nombre" required>
 						
 					</div>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-key"></i></span>
 						</div>
-						<input type="password" class="form-control" placeholder="Contraseña" name="pass" required>
+						<input type="password" class="form-control" placeholder="Contraseña" name="pass" id="pass" required>
 					</div>
 					<div class="row align-items-center remember">
-						<input type="checkbox">Recuérdame
+						<input type="checkbox" id="recuerdame">Recuérdame
 					</div>
 					<div class="form-group">
 						<input type="submit" name="login" value="Login" class="btn float-right login_btn">
