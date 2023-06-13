56e2936a81804914b029704fdd2c7eb9091a1576
Almudena Espinosa <almu.e.c@gmail.com>
2023-06-13 19:02:23 +0200

    excluir includes de las páginas de email

--- server/public/index.php
+++ server/public/index.php
@@ -18,7 +18,13 @@ $rutas = [
     '/politicaPrivacidad' => 'politicaPrivacidad',
     '/politicaCookies' => 'politicaCookies',
     '/guardarPosicion' => 'guardarPosicion',
-    '/insertar' => 'poblarBD'
+    '/insertar' => 'poblarBD',
+    '/calcularIndicadores' => 'calcularIndicadores',
+    '/cerrarSesion' => 'cerrarSesion',
+    '/borrarUsuario' => 'borrarUsuario',
+    '/enviarEmail' => 'enviarEmail',
+    '/cambiarPass' => 'cambiarPass',
+    '/recuperarPass' => 'recuperarPass'
 ];
 
 $env = parse_ini_file("../.env");
@@ -31,7 +37,8 @@ foreach ($env as $key => $value) {
 $ruta_solicitada = $_SERVER['REQUEST_URI'];
 // buscar la ruta en el array de rutas
 if (array_key_exists($ruta_solicitada, $rutas)) {
-    if($ruta_solicitada != 'login' && $ruta_solicitada != 'registro' ){
+    if($ruta_solicitada != '/login' && $ruta_solicitada != '/registro' && $ruta_solicitada != '/registrarUsuario' && $ruta_solicitada != '/inicioSesion' && $ruta_solicitada != '/politicaPrivacidad' 
+    && $ruta_solicitada != '/recuperarPass' && $ruta_solicitada != '/enviarEmail' & $ruta_solicitada != '/cambiarPass' ){
         include('../src/sesion.php');
         include('../src/conexion.php');
         include('../src/actualizarEdificios.php');
