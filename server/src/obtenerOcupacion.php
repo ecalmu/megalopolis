3199e6b73e9ecbd56651cce0a57b32aaf3c437f0
Almudena Espinosa <almu.e.c@gmail.com>
2023-06-13 19:06:53 +0200

    obtener id y nombre asociado a la posicion

--- server/src/obtenerOcupacion.php
+++ server/src/obtenerOcupacion.php
@@ -5,14 +5,22 @@ function obtener_ocupacion($x, $y) {
     $consulta = "SELECT * FROM ubicacion WHERE x = '$x' AND y = '$y'";
     $resultado = mysqli_query($c, $consulta);
     
-
+    
     //Si la consulta devuelve resultados
     if (mysqli_num_rows($resultado) > 0) {
         $resultado = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+        $id = $resultado[0]['id_usuario'];
+
+        //Obtengo el nombre de usuario de la base de datos
+        $consultaNombre = "SELECT nombre FROM usuario WHERE id_usuario = '$id'";
+        $rtdoNombre = mysqli_query($c, $consultaNombre);
+        $rtdoNombre = mysqli_fetch_all($rtdoNombre, MYSQLI_ASSOC);
+
+        $usuario = $rtdoNombre[0]['nombre'];
         if($resultado[0]['tipo'] == 'Ciudad') {
-            return "1";
+            return "1, $usuario, $id";
         }
-        return "2";
+        return "2, $usuario, $id";
     }
     //Si en esa ubicacion no hay nada guardado en la base de datos
     return "0";
