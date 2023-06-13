2c575e4862546d257be2f25fe8a54fed3563c103
Almudena Espinosa <almu.e.c@gmail.com>
2023-06-13 19:15:20 +0200

    borrar comentarios

--- server/public/js/login.js
+++ server/public/js/login.js
@@ -1,49 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-let formulario = document.getElementById("formulario")
-
-formulario.addEventListener("submit", (ev) => {
-    let recuerdame = document.getElementById("recuerdame");
+    let formulario = document.getElementById("formulario")
     
-    //Si el checkbox Recuérdame está marcado...
-    if (recuerdame.checked) {
-        //Obtengo las credenciales del formulario
-        let usuario = document.getElementById("nombre").value;
-        let pass = document.getElementById("pass").value;
+    formulario.addEventListener("submit", (ev) => {
+        let recuerdame = document.getElementById("recuerdame");
         
-        //Calculo la fecha de expiración
-        let fechaActual = new Date();
-        let fechaExpiracion = new Date(fechaActual.getTime() + 1000 * 60 * 60 * 24 * 365);
-
-        // Guardo las credenciales en cookies
-        document.cookie = "usuario=" + usuario + "; expires=" + fechaExpiracion.toUTCString();
-        document.cookie = "pass=" + pass + "; expires=" + fechaExpiracion.toUTCString();
-    } else {
-        //Borro las cookies
-        let fechaBorrar = new Date(1);
-        document.cookie = "usuario=; expires=" + fechaBorrar.toUTCString();
-        document.cookie = "pass=; expires=" + fechaBorrar.toUTCString();
-
-    }
-});
-
-let arrayCookies = document.cookie.split("; ");
+        //Si el checkbox Recuérdame está marcado...
+        if (recuerdame.checked) {
+            //Obtengo las credenciales del formulario
+            let usuario = document.getElementById("nombre").value;
+            let pass = document.getElementById("pass").value;
+            
+            //Calculo la fecha de expiración
+            let fechaActual = new Date();
+            let fechaExpiracion = new Date(fechaActual.getTime() + 1000 * 60 * 60 * 24 * 365);
     
-    for(let cookie of arrayCookies) {
-        //Obtengo por separado el nombre y valor de cada cookie
-        let nombreCookie = cookie.split('=')[0];
-        let valorCookie = cookie.split('=')[1];
-
-        switch(nombreCookie) {
-            case "usuario":
-                document.getElementById('nombre').value = valorCookie;
-                recuerdame.checked = true;
-                break;
-            case "pass":
-                document.getElementById('pass').value = valorCookie;
-                break;
+            // Guardo las credenciales en cookies
+            document.cookie = "usuario=" + usuario + "; expires=" + fechaExpiracion.toUTCString();
+            document.cookie = "pass=" + pass + "; expires=" + fechaExpiracion.toUTCString();
+        } else {
+            //Borro las cookies
+            let fechaBorrar = new Date(1);
+            document.cookie = "usuario=; expires=" + fechaBorrar.toUTCString();
+            document.cookie = "pass=; expires=" + fechaBorrar.toUTCString();
+    
         }
-    }
-})
-// window.addEventListener("load", function() {
+    });
     
-// });
+    let arrayCookies = document.cookie.split("; ");
+        
+        for(let cookie of arrayCookies) {
+            //Obtengo por separado el nombre y valor de cada cookie
+            let nombreCookie = cookie.split('=')[0];
+            let valorCookie = cookie.split('=')[1];
+    
+            switch(nombreCookie) {
+                case "usuario":
+                    document.getElementById('nombre').value = valorCookie;
+                    recuerdame.checked = true;
+                    break;
+                case "pass":
+                    document.getElementById('pass').value = valorCookie;
+                    break;
+            }
+        }
+    })
