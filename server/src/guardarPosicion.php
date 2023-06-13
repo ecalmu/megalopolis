d593e97f52179e097f1cc8dd09225f5f4aaa83cb
Almudena Espinosa <almu.e.c@gmail.com>
2023-06-13 19:05:12 +0200

    añadido coste de los puestos comerciales

--- server/src/guardarPosicion.php
+++ server/src/guardarPosicion.php
@@ -17,7 +17,16 @@
         
         //Si la consulta devuelve resultados
         if (mysqli_num_rows($resultado) > 0) {
-            mysqli_query($c, "INSERT INTO ubicacion (x, y, tipo, id_usuario) VALUES ('$x', '$y', 'Puesto_comercial', $id_usuario)");
+
+            $consulta = "SELECT dinero FROM usuario WHERE id_usuario = $id_usuario";
+            $rtdoUsuario = mysqli_query($c, $consulta);
+            $rtdoUsuario = mysqli_fetch_all($rtdoUsuario, MYSQLI_ASSOC);
+
+            if($rtdoUsuario[0]['dinero'] > 10000) {
+                mysqli_query($c, "INSERT INTO ubicacion (x, y, tipo, id_usuario) VALUES ('$x', '$y', 'Puesto_comercial', $id_usuario)");
+                $dinero = $rtdoUsuario[0]['dinero'] - 10000;
+                mysqli_query($c, "UPDATE usuario SET dinero = '$dinero' WHERE id_usuario = $id_usuario");
+            } 
         } else {
             mysqli_query($c, "INSERT INTO ubicacion (x, y, tipo, id_usuario) VALUES ('$x', '$y', 'Ciudad', $id_usuario)");
         }
