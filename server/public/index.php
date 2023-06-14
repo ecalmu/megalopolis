15cb85a6fc92136f3b920dd6d48d67c7d45b9989
Almudena Espinosa <almu.e.c@gmail.com>
2023-06-15 00:12:59 +0200

    ruta añadida

--- server/public/index.php
+++ server/public/index.php
@@ -38,7 +38,7 @@ $ruta_solicitada = $_SERVER['REQUEST_URI'];
 // buscar la ruta en el array de rutas
 if (array_key_exists($ruta_solicitada, $rutas)) {
     if($ruta_solicitada != '/login' && $ruta_solicitada != '/registro' && $ruta_solicitada != '/registrarUsuario' && $ruta_solicitada != '/inicioSesion' && $ruta_solicitada != '/politicaPrivacidad' 
-    && $ruta_solicitada != '/recuperarPass' && $ruta_solicitada != '/enviarEmail' & $ruta_solicitada != '/cambiarPass' & $ruta_solicitada != '/inicializacion' ){
+    && $ruta_solicitada != '/recuperarPass' && $ruta_solicitada != '/enviarEmail' && $ruta_solicitada != '/cambiarPass' && $ruta_solicitada != '/inicializacion' && $ruta_solicitada != '/insertar'){
         include('../src/sesion.php');
         include('../src/conexion.php');
         include('../src/actualizarEdificios.php');
